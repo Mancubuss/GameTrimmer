32b233a21f94de079ed33796b60e0b73e34ee447
Mancubuss <maxim.polulyakh@gmail.com>
2025-01-06 00:24:54 +0200

    Add core module implementations

---
+++ gametrimmer/core/steam.py
@@ -0,0 +1,12 @@
+"""Steam platform integration module."""
+
+class SteamScanner:
+    """Scanner for Steam games."""
+    
+    def scan_libraries(self):
+        """Scan Steam library folders."""
+        pass
+    
+    def get_installed_games(self):
+        """Get list of installed games."""
+        pass
---
+++ gametrimmer/core/storage.py
@@ -0,0 +1,12 @@
+"""Storage management module."""
+
+class StorageManager:
+    """Manager for temporary storage."""
+    
+    def move_to_storage(self, source, category):
+        """Move file to temporary storage."""
+        pass
+    
+    def restore_from_storage(self, file_id):
+        """Restore file from storage."""
+        pass
